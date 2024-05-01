b92a93d24a77e840d62642619505c9bc4f94556d
PC <PC@MaxStreet-PC>
2024-05-01 20:03:22 -0300

    Se realiza el ejercicio 5

---
+++ tp2_AndreuLuca/src/ar/edu/unju/fi/ejercicio5/model/PagoTarjeta.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.ejercicio5.model;
+import java.time.LocalDate;
+import ar.edu.unju.fi.ejercicio5.interfaces.Pago;
+public class PagoTarjeta implements Pago{
+	private String numeroTarjeta;
+	private LocalDate fechaPago;
+	private double montoPagado;
+	public void realizarPago(double monto) {
+		montoPagado=monto+(monto*0.15);
+	}
+	public void imprimirRecibo() {
+		System.out.println("Numero de Tarjeta: "+numeroTarjeta);
+		System.out.println("Fecha de pago: "+fechaPago);
+		System.out.println("Monto pagado: "+montoPagado);
+	}
+	public PagoTarjeta(String numeroTarjeta, LocalDate fechaPago) {
+		this.numeroTarjeta = numeroTarjeta;
+		this.fechaPago = fechaPago;
+	}
+}
